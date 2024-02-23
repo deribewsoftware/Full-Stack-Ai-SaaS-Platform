3cffe9de94acbaebdceedf4746f944fb07bf638a
deribewsoftware <deribewsoftware@gmail.com>
2024-02-23 11:46:54 +0300

    updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="h-screen justify-center items-center">
-      <UserButton  afterSignOutUrl="/"/>
+     
     </div>
   );
 }
